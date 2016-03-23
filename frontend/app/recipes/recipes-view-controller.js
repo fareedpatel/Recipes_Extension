50d5326307d66809c9fd41b57899daaefbd6b9ed
hedudelgado <hedu.delgado@gmail.com>
2016-03-23 18:33:13 +0000

    api & angular speaking

--- frontend/app/recipes/recipes-view-controller.js
+++ frontend/app/recipes/recipes-view-controller.js
@@ -5,10 +5,14 @@ angular.module('recipesApp.recipes', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/recipes', {
     templateUrl: 'recipes/recipes-view.html',
-    controller: 'View1Ctrl'
+    controller: 'ViewRecipesCtrl'
   });
 }])
 
-.controller('View1Ctrl', [ "$scope", function($scope) {
- $scope.recipes = ['chicken','burger','beer']
+.controller('ViewRecipesCtrl', [ "$scope", "$http", function($scope, $http) {
+ $scope.viewRecipes = function() {
+ 	$http.get('http://localhost:3000/recipes.json').success(function(data){
+ 		$scope.recipes = data
+ 	});
+ }
 }]);
