a2ccf0dcf98a329fd1453b4b9ef94a8024811944
hedudelgado <hedu.delgado@gmail.com>
2016-03-24 13:14:21 +0000

    we can see a list of recipes, we can add a recipe, we have a menu, tests added, added function to allow external connections to rails rack-cors

--- RecipeAPI/app/controllers/application_controller.rb
+++ RecipeAPI/app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ def add_allow_credentials_headers
   response.headers['Access-Control-Allow-Origin'] = '*'
   response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
   response.headers['Access-Control-Request-Method'] = '*'
-  response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override'
 
   response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
   response.headers['Access-Control-Allow-Credentials'] = 'true'
--- frontend/app/recipes/recipes-post-controller.js
+++ frontend/app/recipes/recipes-post-controller.js
@@ -3,16 +3,19 @@
 angular.module('recipesApp.postrecipe', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/recipes', {
-    templateUrl: 'recipes/recipes-post.html',
+  $routeProvider.when('/add-recipe', {
+    templateUrl: 'recipes/add-recipe-form.html',
     controller: 'PostRecipesCtrl'
   });
 }])
 
 .controller('PostRecipesCtrl', [ "$scope", "$http", '$location', function($scope, $http, $location) {
- $scope.postRecipe = function() {
- 	$http.post('http://localhost:3000/recipes.json').success(function(data){
- 		$scope.recipes = data;
+ $scope.postRecipe = function(name) {
+ 	console.log(name)
+ 	var param= {name}
+ 	$http.post('http://localhost:3000/recipes.json', param).success(function(data,status){
+ 		console.log(data)
+ 		console.log(status)
  		$location.path('/recipes');
  	});
  }
