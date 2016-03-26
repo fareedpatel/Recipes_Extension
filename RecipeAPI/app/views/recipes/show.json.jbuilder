f40567445e7458afd2c11bb142485f554ac44569
hedudelgado <hedu.delgado@gmail.com>
2016-03-26 14:57:53 +0000

    starting add a link

--- RecipeAPI/app/views/recipes/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @recipe, :id, :name, :instructions, :created_at, :updated_at
--- RecipeAPI/db/migrate/20160324150839_add_instuctions_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddInstuctionsToRecipes < ActiveRecord::Migration
-  def change
-    add_column :recipes, :instructions, :text
-  end
-end
