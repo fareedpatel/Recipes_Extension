5bb5037fabecc7d62cf055648f1a8a7c40c3d5ab
fareedpatel <fareed_patel@hotmail.co.uk>
2016-03-29 12:53:54 +0100

    wip - testing required for frontend feature

--- RecipeAPI/app/views/recipes/index.json.jbuilder
+++ RecipeAPI/app/views/recipes/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@recipes) do |recipe|
-  json.extract! recipe, :id, :name, :instructions, :preparation_time, :link, :ingredients
+  json.extract! recipe, :id, :name, :ingredients, :instructions, :preparation_time, :link
   json.url recipe_url(recipe, format: :json)
 end
--- frontend/app/recipes/recipes-post-controller.js
+++ frontend/app/recipes/recipes-post-controller.js
@@ -9,8 +9,8 @@ angular.module('recipesApp.postrecipe', ['ngRoute'])
   });
 }])
 .controller('PostRecipesCtrl', [ "$scope", "$http", '$location', function($scope, $http, $location) {
-  $scope.postRecipe = function(name, instructions, preparation_time, link) {
-  	var param= {name, instructions, preparation_time, link}
+  $scope.postRecipe = function(name, instructions, ingredients, preparation_time, link) {
+  	var param= {name, ingredients, instructions, preparation_time, link}
    	$http.post('http://localhost:3000/recipes.json', param).success(function(data,status){
  	  	$location.path('/recipes');
  	  });
