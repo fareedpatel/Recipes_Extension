6a97a275cff32d6b7a024ade52644c41f40ab0c8
hedudelgado <hedu.delgado@gmail.com>
2016-03-31 22:55:04 +0100

    view details form refacted, update controller some refactoring done, all working and passing test

--- frontend/app/recipes/recipes-update-controller.js
+++ frontend/app/recipes/recipes-update-controller.js
@@ -3,17 +3,17 @@
 angular.module('recipesApp.updaterecipe', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/recipes/:recipeId/:recipeName/:recipeInstructions/:recipeIngredients/:recipePreparation_time/edit-recipe', {
+  $routeProvider.when('/recipes/:recipeId/edit-recipe', {
     templateUrl: 'recipes/update-recipe-form.html',
     controller: 'UpdateRecipesCtrl'
   });
 }])
 .controller('UpdateRecipesCtrl', [ "$scope", "$http", '$location', '$routeParams', "UpdateRecipeService", function($scope, $http, $location, $routeParams, UpdateRecipeService) {
-    $scope.name = $routeParams.recipeName
-    $scope.instructions = $routeParams.recipeInstructions
-    $scope.ingredients = $routeParams.recipeIngredients
-    $scope.preparation_time = $routeParams.recipePreparation_time
-    var number = $routeParams.recipeId
+     var number = $routeParams.recipeId
+  $http.get('http://localhost:3000/recipes/' + number + '.json').success(function(data){
+    $scope.recipe = data
+  });
+   
     $scope.editRecipe = function(name, ingredients, instructions, preparation_time, link, callBack) {
     var callBack = function(data,status){
       $location.path('/recipes/'+$routeParams.recipeId);
